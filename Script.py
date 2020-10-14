3ca619d0feef018d729d0b6cbc6a35294c586357
Jucab08h <judithac_vantes@hotmail.com>
2020-10-14 13:47:41 -0700

    script

--- Script.py
+++ Script.py
@@ -6,7 +6,7 @@ from Bio.SeqFeature import FeatureLocation
 from Bio.SeqRecord import SeqRecord
 import os.path
 
-filename = input ("ingrese la dirección de su archivo genbank")
+filename = input ("ingrese la dirección de su archivo genbank: ")
 def summarize_contents(filename):
         record = SeqIO.read(filename, "genbank")
         path = os.path.dirname(filename)
