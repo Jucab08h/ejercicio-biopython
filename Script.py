c8d25d1462240accba7af0ba2d519ea24b566c4b
Jucab08h <judithac_vantes@hotmail.com>
2020-10-14 12:21:00 -0700

    Script arreglado

--- Script.py
+++ Script.py
@@ -1,2 +1,18 @@
-summarize_contents() 
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqFeature import FeatureLocation
+from Bio.SeqRecord import SeqRecord
+import os.path
+
+filename = "/Users/juditharce/Desktop/sequence.gb"
+def summarize_contents(filename):
+        record = SeqIO.read(filename, "genbank")
+        path = os.path.dirname(filename)
+        nregistro = len(filename)
+        
+        print(record.name)
+        print('path:', path)
+        print('num_records:', nregistro)
+        print(record.id)
+        
 
