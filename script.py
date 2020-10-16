a383d3c6dad747ba42d20a53805d146c2d592a51
Jucab08h <judithac_vantes@hotmail.com>
2020-10-16 09:23:29 -0700

    completo

--- script.py
+++ script.py
@@ -12,12 +12,16 @@ def summarize_contents(filename):
         print("file:", file)
         print("path:", path)
        
-        num_records = list(SeqIO.parse (filename, "genbank"))
-        print("num_records: %i records" % len(num_records))
+        num_records = list(SeqIO.parse(filename, "genbank"))
+        print("num_records: %i " % len(num_records))
         print("records: ")
-	
+        
+        records = []
+        for Seq_Record in SeqIO.parse(filename, "genbank"):
+                records.append(Seq_Record) #agregamos otro a la lista
+                print("- id: ", Seq_Record.id)
+                print("  name: ", Seq_Record.name)
+                print("  description: ", Seq_Record.description)
 
-
-
-summarize_contents(filename)        
+summarize_contents(filename)
 
