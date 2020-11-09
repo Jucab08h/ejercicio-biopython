febb7b6618490f1af65ee9fdeab4bc5d927a2284
Jucab08h <judithac_vantes@hotmail.com>
2020-11-09 04:57:17 -0700

    Se agregó una nueva fución a script.py, la cual recibe dos cadenas de ADN, las concatena y genera un complementario inverso con ayuda de la librería de biopython.  [biopython-tarea2-ex1]

--- script.py
+++ script.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 
+from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-import os
+import os.path
 
-#dentro de las comillas va la dirección del archivo genbank
-filename =  "/Users/juditharce/Desktop/ls_orchid.gbk"
-
+filename =  os.path.abspath("data/ls_orchid.gbk")
 def summarize_contents(filename):
     file = os.path.basename(filename)
     path = os.path.dirname(filename)
+    tipexten = os.path.splitext(filename)
+    if(tipexten[1] == ".gbk"):
+        tipo = "genbank"
+    else:
+        tipo = "fasta"
+        pass
     num_records = list(SeqIO.parse(filename, "genbank"))
     
 #el diccionario 
-    Dic = {}
-    
+    Dic = {}    
     Dic['file:'] = file
     Dic['path:'] = path
     Dic['num_records:'] = len(num_records)
@@ -28,7 +32,19 @@ def summarize_contents(filename):
         Dic['id:'].append(Seq_Record.id)
         Dic['descriptions:'].append(Seq_Record.description)
     return Dic
-
 if __name__ == "__main__":
     resultado = summarize_contents(filename)
     print(resultado)
+
+
+        #secuencias que se desean concatenar 
+Seq1 = Seq("agtgagactcgaggg")
+Seq2 = Seq("AgTgAGAaaTtT")
+
+def concatenate_and_get_reverse_of_complement(Seq1, Seq2):
+    secuencia = Seq1 + Seq2  #concatenar 
+    inverso = secuencia.reverse_complement() #invertir 
+
+    print(inverso)
+
+concatenate_and_get_reverse_of_complement(Seq1, Seq2)
